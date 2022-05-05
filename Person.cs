30a5fe712dec70547e54ca3000592b25e7b973a6
Krzysztof <programowanie.krzysiek@gmail.com>
2022-05-05 15:22:39 +0200

    v1

---
+++ Person.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LanguagesDuel03JavaVsCsharpStreamsLinqCsharpCode
+{
+    public readonly record struct Person(string Name, int Age)
+    {
+        public bool HasAgeBetween(int ageFrom, int ageTo) 
+            => ageFrom <= Age && Age <= ageTo;
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿
+using LanguagesDuel03JavaVsCsharpStreamsLinqCsharpCode;
+
+/*
+ * Z podanej kolekcji osób wyznacz imiona tych, 
+ * które posiadają wiek w określonym przedziale.
+ */
+
+List<Person>? people = new()
+{
+    new Person("ADAM", 34),
+    new Person("IZA", 21),
+    new Person("JAN", 39),
+    new Person { Name = "KAMIL", Age = 32 },
+    new Person(Name: "EWA", Age: 18)
+};
+
+// LINQ - Method Syntax
+var names1 = people
+    .Where(p => p.HasAgeBetween(30, 39))
+    .Select(p => p.Name)
+    .Aggregate((n1, n2) => $"{n1}, {n2}");
+
+Console.WriteLine(names1);
+
+// LINQ - Query Syntax
+var names = from p in people
+            where p.HasAgeBetween(30, 39)
+            select p.Name;
+
+var names2 = String.Join(", ", names);
+Console.WriteLine(names2);
+
+// LINQ - Mixed Syntax
+
+var names3 = (
+    from p in people
+    where p.HasAgeBetween(30, 39)
+    select p.Name)
+    .Aggregate((n1, n2) => $"{n1}, {n2}");
+
+Console.WriteLine(names3);
